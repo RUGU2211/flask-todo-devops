3abae661e723632dc9160ffb904fb93035fbcfea
Rugved Kharde <rugvedkharde2211@gmail.com>
2025-08-04 22:31:18 +0530

    Added To-Do form frontend in master_1

--- flask_app/app.py
+++ flask_app/app.py
@@ -1,1 +1,10 @@
-from flask import Flask, jsonify, request; app = Flask(__name__); @app.route('/')\ndef home(): return 'Flask Running'
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return render_template('todo_form.html')
+
+if __name__ == "__main__":
+    app.run(debug=True, port=5004)
